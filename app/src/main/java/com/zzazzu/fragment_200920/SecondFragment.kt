31d9f7e8f2a32b2445d1c83971809cb6b78080ca
bucephalus41 <bucephalus41@gmail.com>
2020-09-26 10:37:28 +0900

    1037

---
+++ app/src/main/java/com/zzazzu/fragment_200920/SecondFragment.kt
@@ -0,0 +1,20 @@
+package com.zzazzu.fragment_200920
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class SecondFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.activity_fragment_second, container, false)
+    }
+
+
+}
---
+++ app/src/main/java/com/zzazzu/fragment_200920/fragment_second.kt
@@ -0,0 +1,31 @@
+package com.zzazzu.fragment_200920
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.activity_fragment_second.*
+
+class SecondFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.activity_fragment_second, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        toastBtn.setOnClickListener{
+            Toast.makeText(context, "버튼이 눌림", Toast.LENGTH_SHORT).show()
+
+        }
+
+    }
+
+}
