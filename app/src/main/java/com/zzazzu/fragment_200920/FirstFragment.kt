e2b51db45bd56551f8d8b2d4f215b9c7c089dec0
bucephalus41 <bucephalus41@gmail.com>
2020-09-26 10:07:19 +0900

    프래그먼트 상 이벤트 추가?

--- app/src/main/java/com/zzazzu/fragment_200920/FirstFragment.kt
+++ app/src/main/java/com/zzazzu/fragment_200920/FirstFragment.kt
@@ -5,6 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.frament_test.*
+
+//fragment 함수는 fragment 안에서만 함수 만들어 사용한다.
+//frag 는 activity 위에 얹어지는 조각같은 것
 
 class FirstFragment : Fragment() {
 
@@ -13,7 +17,17 @@ class FirstFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.frament_test)
+        return inflater.inflate(R.layout.frament_test, container, attachToRoot false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        changeTextBtn.setOnClickListener{
+
+            firstTxt.text = "버튼이 눌림"
+
+        }
     }
 
 }
